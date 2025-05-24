6dcc5d4125d90fd91ab3572a71d17692c7007e3e
Abdullah Ansari <abdullahansari018@gmail.com>
2025-05-24 19:14:13 +0530

    Add StockListView and integrate with ViewController

--- SwiftUIKitIntegration/SwiftUIKitIntegration/ViewController.swift
+++ SwiftUIKitIntegration/SwiftUIKitIntegration/ViewController.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onMoveToStock(_ sender: UIButton) {
+        
+        let vc = UIHostingController(rootView: StockListView())
+        self.navigationController?.pushViewController(vc, animated: true)
     }
 
 
---
+++ SwiftUIKitIntegration/SwiftUIKitIntegration/Views/StockListView.swift
@@ -0,0 +1,41 @@
+//
+//  StockListView.swift
+//  SwiftUIKitIntegration
+//
+//  Created by Abdullah Ansari on 24/05/25.
+//
+
+import SwiftUI
+
+struct Stock: Identifiable {
+    let name: String
+    let price: Double
+    let id = UUID()
+}
+
+struct StockListView: View {
+    
+    let stocks = [
+                 Stock(name: "MSFT", price: 250),
+                 Stock(name: "AAPL", price: 104.56),
+                 Stock(name: "TSLA", price: 450),
+                 Stock(name: "AMZN", price: 120.00),
+                 
+    
+    ]
+    
+    var body: some View {
+        List(stocks) { stock in
+            HStack {
+                Text(stock.name)
+                Spacer()
+                Text(stock.price, format: .currency(code: "USD"))
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    StockListView()
+}
